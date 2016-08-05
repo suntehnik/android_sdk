6dd5b907d8b8da715e4b46fddebe6a1d16f95091
Pedro <pedro@adeven.com>
2016-08-05 17:14:20 +0200

    Add debug comments

--- Adjust/adjust/src/main/java/com/adjust/sdk/CustomScheduledExecutorService.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/CustomScheduledExecutorService.java
@@ -7,11 +7,15 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by pfms on 05/08/2016.
  */
 public class CustomScheduledExecutorService extends ScheduledThreadPoolExecutor {
+//    private AtomicInteger threadCounter = new AtomicInteger(1);
+//    private String source;
+
     public CustomScheduledExecutorService(final String source) {
         super(1,                                        // Single thread
                 new ThreadFactory() {                   // Creator of daemon threads
@@ -37,35 +41,50 @@ public class CustomScheduledExecutorService extends ScheduledThreadPoolExecutor 
                     }
                 }
         );
+//        this.source = source;
         super.allowCoreThreadTimeOut(true);
     }
 
     @Override
     public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+//        AdjustFactory.getLogger().verbose("CustomScheduledExecutorService schedule from %s source, with %d delay", source, delay);
         return super.schedule(new RunnableWrapper(command), delay, unit);
     }
 
     @Override
     public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+//        AdjustFactory.getLogger().verbose("CustomScheduledExecutorService scheduleWithFixedDelay from %s source, with %d delay and %d initial delay",
+//                source, delay, initialDelay);
         return super.scheduleWithFixedDelay(new RunnableWrapper(command), initialDelay, delay, unit);
     }
 
     @Override
     public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+//        AdjustFactory.getLogger().verbose("CustomScheduledExecutorService scheduleWithFixedDelay from %s source, with %d delay and %d period",
+//                source, period, initialDelay);
         return super.scheduleAtFixedRate(new RunnableWrapper(command), initialDelay, period, unit);
     }
 
     private class RunnableWrapper implements Runnable {
         private Runnable runnable;
+//        private long created;
+//        private int threadNumber;
 
         public RunnableWrapper(Runnable runnable) {
             this.runnable = runnable;
+//            created = System.currentTimeMillis();
+//            threadNumber = threadCounter.getAndIncrement();
+//            AdjustFactory.getLogger().verbose("RunnableWrapper %d from %s created at %d", threadNumber, source, created);
         }
 
         @Override
         public void run() {
             try {
+//                long before = System.currentTimeMillis();
+//                AdjustFactory.getLogger().verbose("RunnableWrapper %d from %s source, before running at %d", threadNumber, source, before);
                 runnable.run();
+//                long after = System.currentTimeMillis();
+//                AdjustFactory.getLogger().verbose("RunnableWrapper %d from %s source, after running at %d", threadNumber, source, after);
             } catch (Throwable t) {
                 AdjustFactory.getLogger().error("Runnable error %s", t.getMessage());
             }
